d993ce2dc1f9e6d2cdc662103842bc28a8febfdd
Mariel <marielterr@dhcp-rhodes-1432.eduroam.cornell.edu>
2016-11-28 18:16:43 -0500

    updated css

---
+++ includes/footer.php
@@ -0,0 +1,17 @@
+<?php
+	echo '
+	<div id="footer">
+		<ul>
+			<li class = "facebook">
+				<a href = "https://www.facebook.com/downtownithaca" target = "_blank"><span>Placeholder</span></a>
+			</li>
+			<li class = "twitter">
+				<a href = "https://www.twitter.com/downtownithaca" target = "_blank"><span>Placeholder</span></a>
+			</li>
+			<li class = "youtube">
+				<a href = "https://www.youtube.com/downtownithaca" target = "_blank"><span>Placeholder</span></a>
+			</li>
+		</ul>
+	</div>
+	';
+?>
