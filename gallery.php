19f8b7f8b5bd854aef16f29d730e63fd2599a035
Amy Zhong <AmyZhong@dhcp-rhodes-1342.redrover.cornell.edu>
2016-12-03 16:44:09 -0500

    gallery page update

--- gallery.php
+++ gallery.php
@@ -26,6 +26,16 @@
 		</div> <!-- end of navbar div -->
 	</div> <!-- end of stickyheader div -->
 	<div id = "main">
+		<h1>Our Photos</h1>
+		<img src="images/apple_fest(1).jpeg" alt="Apple Fest">
+		<img src="images/DSC_0510(1).JPG" alt="Maple Syrup">
+		<img src="images/DSC_0687(1).JPG" alt="Apples">
+		<img src="images/IMG_1334.jpg" alt="Food Stand">
+		<img src="images/IMG_1345.jpg" alt="Maple Syrup Stand">
+		<img src="images/IMG_1348.jpg" alt="Food Stand">
+		<img src="images/IMG_1353.jpg" alt="Lemonade Stand">
+		<img src="images/IMG_1359.jpg" alt="Corn Stand">
+		<img src="images/IMG_1361.jpg" alt="Berry Stand">
 	</div> <!-- end of main div -->
 	<div id = "twitfeed">
 	<a class="twitter-timeline" data-width="220" data-height="800" href="https://twitter.com/downtownithaca">Tweets by downtownithaca</a> <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
