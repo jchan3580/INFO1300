150c6496b7b553ca8f2f4867b91fdc5c9b3ef4c0
Amy Zhong <AmyZhong@dhcp-rhodes-1342.redrover.cornell.edu>
2016-12-03 15:07:08 -0500

    change index

--- index.php
+++ index.php
@@ -42,11 +42,13 @@
 			<button onclick="change(0);">Previous</button>
         	<button onclick="change(1);">Next</button>
 		</div>-->
-		<div id="photos">
+		<div id= "photo1">
 			<img src="images/apple_fest.jpeg" alt="Apple Fest">
-            <img src="images/DSC_0510.JPG" alt="Maple Syrup">
-            <img src="images/DSC_0687.JPG" alt="Apples">
 		</div>
+		<div id="photo2">
+            <img src="images/DSC_0510.JPG" alt="Maple Syrup">
+            <img src="images/DSC_0687.JPG" alt="Apples">\
+        </div>
 		<div id="content">
 			<h1>About</h1>
 				<p>For 34 years, Ithaca, NY has hosted plenty of apples, local produce, fresh baked goods, fun entertainment and live performances on two stages. The Apple Harvest Festival is always a great introduction to the rich farm and artist community of Ithaca.</p> 
