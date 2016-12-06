992aff5011e4cd79ec5e443a50aa2ff767a5aa88
jchan3580 <jchan3580@gmail.com>
2016-12-06 12:08:36 -0500

    twitterm.php

---
+++ includes/twitterm.php
@@ -0,0 +1,19 @@
+<?php
+	echo '
+	<div id="twitterm">
+		<div style="display:none;" id="feed">
+			<a class="twitter-timeline" data-width="220" data-height="400" href="https://twitter.com/downtownithaca">Tweets by downtownithaca</a> <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+		</div>
+		<div style="display:block;" id="show">
+			<a href = "#" onclick = "document.getElementById(\'feed\').style.display=\'block\'; 
+									 document.getElementById(\'show\').style.display=\'none\';
+									 document.getElementById(\'hide\').style.display=\'block\'; return false;">Click here for tweets from Downtown Ithaca!</a>
+		</div>
+		<div style="display:none;" id="hide">
+			<a href = "#" onclick = "document.getElementById(\'feed\').style.display=\'none\'; 
+									 document.getElementById(\'show\').style.display=\'block\';
+									 document.getElementById(\'hide\').style.display=\'none\'; return false;">Click here to hide tweets from Downtown Ithaca!</a>
+		</div>
+	</div>
+	';
+?>
