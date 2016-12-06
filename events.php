ea446904696a4248e5a5bebf02842b44f4046b2c
Mariel <marielterr@dhcp-gs-2720.eduroam.cornell.edu>
2016-12-06 14:48:25 -0500

    asdf

--- events.php
+++ events.php
@@ -132,8 +132,9 @@
         </div>
 	</div> <!-- end of main div -->
     <!--events schedule information source: http://www.downtownithaca.com/ithaca-events/Apple%20Harvest%20Festival%20Presented%20by%20Tompkins%20Trust -->
-	<div id = "twitfeed">
-	<a class="twitter-timeline" data-width="220" data-height="800" href="https://twitter.com/downtownithaca">Tweets by downtownithaca</a> <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
-	</div> <!-- end of twitfeed div -->
+    
+    <div id = "twitfeed">
+        <a class="twitter-timeline" data-width="220" data-height="800" href="https://twitter.com/downtownithaca">Tweets by downtownithaca</a> <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+    </div> <!-- end of twitfeed div -->
 </body>
 </html>
