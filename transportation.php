a7a3b0a3576a86f467da1c9b321105fe4d9c6ffa
Amy Zhong <AmyZhong@dhcp-gs-2348.redrover.cornell.edu>
2016-12-07 21:39:02 -0500

    fixed frameborder error

--- transportation.php
+++ transportation.php
@@ -34,7 +34,7 @@
     <h1>Transportation</h1>
 	<div id="map">
 		<!-- credit for the map given to Google Maps -->
-		<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5888.801242822939!2d-76.50103518900127!3d42.44049143139099!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89d0819c7e2b29ff%3A0x9a002546d78a1ced!2s171+E+State+St%2C+Ithaca%2C+NY+14850!5e0!3m2!1sen!2sus!4v1480481482106" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+		<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5888.801242822939!2d-76.50103518900127!3d42.44049143139099!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89d0819c7e2b29ff%3A0x9a002546d78a1ced!2s171+E+State+St%2C+Ithaca%2C+NY+14850!5e0!3m2!1sen!2sus!4v1480481482106" width="600" height="450" style="border:0" allowfullscreen></iframe>
 	
 	</div> <!-- end of map div -->
         <div id = "tcat">
